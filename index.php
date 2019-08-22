b21655406b43fe425333aa243723fe92c4f0a256
jody-davids-07 <Your Actual Email>
2019-08-22 13:40:32 +0100

    Added title and heading names

--- index.php
+++ index.php
@@ -9,3 +9,9 @@
     <link href="css/styles.css" rel="stylesheet" type="text/css">
         <title>My quiz app</title>
     </head>
+    <body>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            <div class="app">
+            <section>
+            <h1>How much do you know about our earth?</h1>
+            </section>

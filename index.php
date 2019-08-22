57635e9f92aed23e9ca64e5518f711a336a085b1
jody-davids-07 <Your Actual Email>
2019-08-22 13:44:37 +0100

    Added comment and 10 questions and 10 possible answers

--- index.php
+++ index.php
@@ -16,5 +16,95 @@
             <h1>How much do you know about our earth?</h1>
             </section>
             <?php
-             
+              // questions asked and possible answers
+              $quiz[] = [
+                'question' => 'what is the largest living animal',
+                'answer' => [
+                    'elephant',
+                    'blue_whale',
+                    'giraffe',
+                    'buffalo'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'Against which illness do you use insuline',
+                'answer' => [
+                    'high_blood_pressure',
+                    'adiposity',
+                    'diabetes',
+                    'cardiac_infection'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'what is the biggest city in USA according to the mass of metropolitan area?',
+                'answer' => [
+                    'Los Angeles',
+                    'New york',
+                    'chicago',
+                    'san diego'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'which word does the "e" in e-mail stand for?',
+                'answer' => [
+                    'explain',
+                    'electronic',
+                    'execute',
+                    'exact'
+                    ]
+                ];
+                $quiz[] = [
+                'question' => 'how much % of the world is covered with water?',
+                'answer' => [
+                    '92%',
+                    '63%',
+                    '37%',
+                    '71%'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'which is the longest river in the world' ,
+                'answer' => [
+                    'nile',
+                    'congo',
+                    'mississipi',
+                    'amazon'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'how many teeth does an adult human usually have?' ,
+                'answer' => [
+                    '12-16',
+                    '16-24',
+                    '28-32',
+                    '32-36'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'who developed the theory of evolution?' ,
+                'answer' => [
+                    'Isaac Newton',
+                    'Charles Darwin',
+                    'Pythagore',
+                    'Davinci'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'which one of this animal is a fish?' ,
+                'answer' => [
+                    'Shark',
+                    'Whale',
+                    'Dolphin',
+                    'Seal'
+                ]
+                ];
+                $quiz[] = [
+                'question' => 'which one of these numbers is without rest divisable through the numbers : 1,2,3,4,5,6?' ,
+                'answer' => [
+                    '42',
+                    '40',
+                    '60',
+                    '128'
+                ]
+                ]; 
             ?>

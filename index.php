ea651619fa19739dbda2328f3af4871633369811
jody-davids-07 <Your Actual Email>
2019-08-22 13:48:17 +0100

    Added arrays, comments and for loop to encrypt input from user

--- index.php
+++ index.php
@@ -196,5 +196,11 @@
                         'oblate spheroid',
                         'none of the above'
                     ]
-                    ]; 
+                    ];
+                    // arrays
+                    $answerName = array('question1','question2','question3','question4','question5','question6','question7','question8','question9','question10', 'question11', 'question12', 'question13', 'question14', 'question15', 'question16', 'question17', 'question18', 'question19', 'question20');
+                    $number = array('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20');
+                    for($i = 0; $i < count($quiz); ++$i){
+                    $outputquestion = "<h3>".$number[$i].')'.$quiz[$i]['question']."</h3>";
+                    echo $outputquestion; 
             ?>
